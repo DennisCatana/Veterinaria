93bcdf2b53e398d01a425fe275b25b12e0c8ba00
DennisCatana <dennis.joel235@gmail.com>
2024-01-22 14:18:43 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,50 @@
+// Requerir los módulos
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors';
+import routerVeterinarios from './routers/veterinario_routes.js'
+import routerPacientes from './routers/paciente_routes.js'
+
+
+
+
+
+// Inicializaciones
+const app = express()
+dotenv.config()
+
+
+// Configuraciones 
+app.set('port',process.env.port || 3000)
+app.use(cors())
+
+
+
+
+// Middlewares 
+app.use(express.json())
+
+
+// Variables globales
+
+
+
+
+// Rutas 
+app.get('/',(req,res)=>{
+    res.send("Server on")
+})
+//ruta para veterinarios
+app.use('/api',routerVeterinarios)
+
+//Ruta para pacientes
+app.use('/api',routerPacientes)
+
+
+// Manejo de una ruta que no sea encontrada
+app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
+
+
+
+// Exportar la instancia de express por medio de app
+export default  app
