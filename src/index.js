56f49d1eddbed355d120b3ead80d25cebadacd0f
DennisCatana <dennis.joel235@outlook.es>
2024-02-10 16:14:22 -0500

    xd

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-// Importar la aplicación express
-import app from './server.js';
+//iMPORAT LA VARIABLE APP
+import app from './server.js'
 
-// Importar la función para la conexión con la base de datos
+//Importando la funcion con la BDD
 import connection from './database.js';
-
-// Llamar a la función para establecer la conexión con la base de datos
-connection();
 
 // Obtener el puerto desde la configuración de la aplicación
 const port = process.env.PORT || 3000;
 
-// Iniciar el servidor
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+//llamando la funcion connection()
+connection()
+
+
+app.listen(app.get('port'),()=>{
+    console.log(`Server ok on http://localhost:${port}`);
+})
+
+
+
+
